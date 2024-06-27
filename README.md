a64d13684a36ac14bac03f7de6d5aaf7f10f93ff
RithikRajVemula <150193329+RithikRajVemula@users.noreply.github.com>
2024-06-27 12:22:19 -0400

    Add base code

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# vue_frontend
+# vue-resumegeneration-teller
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+// import MenuBar from "./components/MenuBar.vue";
+</script>
+
+<template>
+  <v-app :style="{ background: '#F4F4F4' }">
+    <MenuBar :key="$route.fullPath" />
+    <h3 :style="{ color: '#000000' }"> Home page for Resume Generation</h3>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
