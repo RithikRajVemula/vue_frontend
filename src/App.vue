50ce986ea8810e6904ec915ecce437e97810f277
likhithabattula <166478299+likhithabattula@users.noreply.github.com>
2024-07-02 13:26:47 -0400

    remove h3 tag from App.vue file

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup>
-// import MenuBar from "./components/MenuBar.vue";
+import MenuBar from "./components/MenuBar.vue";
 </script>
 
 <template>
   <v-app :style="{ background: '#F4F4F4' }">
     <MenuBar :key="$route.fullPath" />
-    <h3 :style="{ color: '#000000' }"> Home page for Resume Generation</h3>
     <v-main>
       <router-view />
     </v-main>
